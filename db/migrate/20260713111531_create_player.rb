c6682978f642b2f2b8491df5171e409afb16aa5a
Davis Okoth <davisokoth@gmail.com>
2026-07-14 13:05:56 +0300

    Fix create players migration
    The migration had a typo in the index creation, and the columns were not marked as null: false

--- db/migrate/20260713111531_create_player.rb
+++ db/migrate/20260713111531_create_player.rb
@@ -1,13 +1,14 @@
 class CreatePlayer < ActiveRecord::Migration[8.1]
   def change
     create_table :players do |t|
-      t.string :code
-      t.string :name
-      t.date :date_of_birth
-      t.string :country_of_origin
+      t.text :code, null: false
+      t.text :name, null: false
+      t.date :date_of_birth, null: false
+      t.text :country_of_origin, null: false
 
       t.timestamps
     end
-    add_index:palyers, :code,unique:true
+
+    add_index :players, :code, unique: true
   end
 end
