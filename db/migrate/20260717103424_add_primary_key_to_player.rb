dd5c012d7d896c9fcd308ed38f13d7ac7b04b3e6
TobiasOmollo <237821654+TobiasOmollo@users.noreply.github.com>
2026-07-19 02:32:10 +0300

    Replace players id primary key with code

--- db/migrate/20260717103424_add_primary_key_to_player.rb
+++
@@ -1,9 +0,0 @@
-class AddPrimaryKeyToPlayer < ActiveRecord::Migration[8.1]
-  def up
-    execute "ALTER TABLE players ADD PRIMARY KEY (code);"
-  end
-
-  def down
-    execute "ALTER TABLE players DROP CONSTRAINT players_pkey;"
-  end
-end
