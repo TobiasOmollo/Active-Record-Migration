a0a0c430a1a578b61d41d45816a83438a36faae6
TobiasOmollo <237821654+TobiasOmollo@users.noreply.github.com>
2026-07-22 14:40:22 +0300

    Set id to uuid type generation by default

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_22_111142) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_22_113717) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -37,12 +37,11 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_22_111142) do
     t.index ["tournament_id"], name: "index_matches_on_tournament_id"
   end
 
-  create_table "players", id: false, force: :cascade do |t|
+  create_table "players", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.text "code", null: false
     t.text "country_of_origin", null: false
     t.datetime "created_at", null: false
     t.date "date_of_birth", null: false
-    t.uuid "id"
     t.text "name", null: false
     t.integer "status"
     t.datetime "updated_at", null: false
